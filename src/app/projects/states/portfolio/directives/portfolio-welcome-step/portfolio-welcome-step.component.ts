b4e39a1966ed4f3d25398bf321aa9b622a9c75aa
leocomsci <allu@deakin.edu.au>
2022-04-24 01:39:27 +1000

    refactor: create initial files for migration of portfolio-welcome-step

---
+++ src/app/projects/states/portfolio/directives/portfolio-welcome-step/portfolio-welcome-step.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-welcome-step',
+  templateUrl: 'portfolio-welcome-step.component.html',
+})
+export class TaskDescriptionCardComponent {
+  constructor() { }
+}
