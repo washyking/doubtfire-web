eb326b68cdb8edaea1173216fca579555c48c0c2
Burchonator <mitchell.burcheri@gmail.com>
2022-05-07 20:21:17 +1000

    refactor: create initial files for migration of create-unit-modal

---
+++ src/app/admin/modals/create-unit-modal/create-unit-modal.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'create-break-modal',
+  templateUrl: 'create-break-modal.component.html',
+})
+export class CreateBreakModalComponent {
+  constructor() {}
+}
