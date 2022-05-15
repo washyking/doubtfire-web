b85a716482a15966861b4f864eba92790cc6016b
oscarcow1 <caideq@deakin.edu.au>
2022-05-15 13:15:01 +0800

    fix: add button can be use

--- src/app/admin/modals/user-settings-modal/user-settings-modal.component.ts
+++ src/app/admin/modals/user-settings-modal/user-settings-modal.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { currentUser } from 'src/app/ajs-upgraded-providers';
+import { currentUser, alertService } from 'src/app/ajs-upgraded-providers';
 import { User, UserService } from 'src/app/api/models/doubtfire-model';
 import { DoubtfireConstants } from 'src/app/config/constants/doubtfire-constants';
 
@@ -13,13 +13,27 @@ export class UserSettingsModalComponent implements OnInit {
   // create or not
   isNew: boolean = true;
   // user information list
-  user: User | null;
+  user: User = new User({
+    username: '',
+    first_name: '',
+    last_name: '',
+    nickname: '',
+    email: '',
+    opt_in_to_research: false,
+    system_role: '',
+    receive_task_notifications: false,
+    receive_portfolio_notifications: false,
+    receive_feedback_notifications: false,
+    has_run_first_time_setup: false
+  });
+  users: User[] = new Array<User>();
 
-  externalName: string;
+  externalName: string = '';
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     @Inject(currentUser) public CurrentUser: any,
+    @Inject(alertService) private alerts: any,
     public dialogRef: MatDialogRef<UserSettingsModalComponent>,
     private constants: DoubtfireConstants,
     private userService: UserService
@@ -40,7 +54,13 @@ export class UserSettingsModalComponent implements OnInit {
       // this.userService.query({ id: this.data.id }).subscribe((data) => {
       //   this.user = data[0]
       // })
+    } else { // add
+      this.userService.query().subscribe(data => {
+        this.users = data;
+      })
+
     }
+
   }
 
   // create new user
@@ -49,6 +69,11 @@ export class UserSettingsModalComponent implements OnInit {
       user: this.user
     }).subscribe(response => {
       this.dialogRef.close();
+      if (this.users){
+        return this.users.push(response);
+      }
+    }, (err) => {
+      this.alerts.add('danger', err?.error?.error, 3000);
     })
   }
 
@@ -58,6 +83,8 @@ export class UserSettingsModalComponent implements OnInit {
     this.userService.update(this.currentWebcalWith(this.user)).subscribe(response => {
       this.userService.save(response)
       this.dialogRef.close();
+    }, (err) => {
+      this.alerts.add('danger', err?.error?.error, 3000);
     })
   }
 
