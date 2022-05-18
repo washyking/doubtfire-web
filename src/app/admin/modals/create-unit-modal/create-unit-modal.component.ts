2028c17c6f193f473178bf80b954d336f315a0eb
Burchonator <mitchell.burcheri@gmail.com>
2022-05-18 22:26:04 +1000

    refactor: added comments

--- src/app/admin/modals/create-unit-modal/create-unit-modal.component.ts
+++ src/app/admin/modals/create-unit-modal/create-unit-modal.component.ts
@@ -1,17 +1,27 @@
-// create-unit-modal
+// create-unit-modal component
+// Migrated by Mitchell Burcheri
+// Help from Ray Guo
+// Migrated Semester 1 2022
+
+// Importing modules
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { analyticsService, Unit, alertService } from 'src/app/ajs-upgraded-providers';
 import { DoubtfireConstants } from 'src/app/config/constants/doubtfire-constants';
 
+// Angular component decorator
 @Component({
   selector: 'df-create-break-modal',
   templateUrl: 'create-unit-modal.component.html',
 })
+
+// CreateUnitModalComponent for exporting to other programs
 export class CreateUnitModalComponent implements OnInit {
+  // Defining variables
   units: any = this.data.units;
   unit: any = { code: null, name: null };
   constructor(
+    // data being injected such as: CreateUnitModalComponent(analyticsService,Unit,alertService,DoubtfireConstants,MAT_DIALOG_DATA)
     @Inject(analyticsService) private analyticsService: any,
     @Inject(Unit) private Unit: any,
     @Inject(alertService) private alertService: any,
@@ -20,24 +30,31 @@ export class CreateUnitModalComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
   ngOnInit(): void {
+    // Runs when task is run
+    // Analytics Service event is created
     this.analyticsService.event('Unit Admin', 'Started to Create Unit');
-    console.log(this.data);
   }
   saveUnit() {
+    // Saves unit using the inputted unit code and unit name
     this.Unit.create(
+      // Create unit using the unit code and unit name. This is the inputted data.
       { unit: this.unit },
+      // response is the data inputted
       (response) => {
-        console.log('success');
+        // If successful, log unit creation is successful and push unit to units
         this.alertService.add('success', 'Unit created.', 2000);
+        // closes dialog box
         this.dialogRef.close();
+        // pushes unit code and unit name to list of units
         this.units.push(response);
         this.analyticsService.event('Unit Admin', 'Saved New Unit');
       },
-      (response) => {
-        console.log('err');
+      () => {
+        // If unsuccessful, log that there is an error creating unit
         this.alertService.add('danger', 'Error creating unit - #{response.data.error}');
       }
     );
   }
+  // The line below is required for this task to compile
   externalName = this.DoubtfireConstants.ExternalName;
 }
--- src/app/admin/modals/create-unit-modal/create-unit-modal.service.ts
+++ src/app/admin/modals/create-unit-modal/create-unit-modal.service.ts
@@ -1,15 +1,22 @@
-// create-unit-modal
+// create-unit-modal service
+// Migrated by Mitchell Burcheri
+// Help from Ray Guo
+// Migrated Semester 1 2022
+
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { CreateUnitModalComponent } from './create-unit-modal.component';
 
 @Injectable({
+  // creates a provider for the service
   providedIn: 'root',
 })
 export class CreateUnitModalService {
   constructor(public dialog: MatDialog) {}
   show(units): void {
+    // show dialog box for CreateUnitModalComponent
     const dialogRef = this.dialog.open(CreateUnitModalComponent, {
+      // the data being collected is the unit code and unit name
       data: { units },
     });
   }
