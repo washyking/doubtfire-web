848ac6fc07182b0ce1b6da4de4693869d977aded
Coskun Kilinc <82747562+coskun-kilinc@users.noreply.github.com>
2024-03-25 10:34:56 +1100

    feat: add task definition stages component
    Adds feedbacks tages component to task definitions for tutor feedback
    enhancement feature.

---
+++ src/app/units/states/edit/directives/unit-tasks-editor/task-definition-editor/task-definition-stages/task-definition-stages.component.ts
@@ -0,0 +1,49 @@
+import {Component, Input, ViewChild} from '@angular/core';
+import {MatTable} from '@angular/material/table';
+import {TaskDefinition} from 'src/app/api/models/task-definition';
+import {Unit} from 'src/app/api/models/unit';
+import {Stage} from 'src/app/api/models/task-definition';
+@Component({
+  selector: 'f-task-definition-stages',
+  templateUrl: 'task-definition-stages.component.html',
+  styleUrls: ['task-definition-stages.component.scss'],
+})
+export class TaskDefinitionStagesComponent {
+  @Input() public taskDefinition: TaskDefinition;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  @ViewChild('stageTable', {static: true}) table: MatTable<any>;
+
+  public columns: string[] = ['title', 'order', 'criteria', 'cases', 'row-actions'];
+
+  public get unit(): Unit {
+    return this.taskDefinition?.unit;
+  }
+
+  public addStage() {
+    // check if the stages proprty exists in the taskDefinition object
+
+    if (!('stages' in this.taskDefinition)) {
+      // cannot add a stage if the stages property does not exist
+      console.error('Cannot add a stage if the stages property does not exist');
+      // exit the function
+      return;
+    }
+
+    const newLength = this.taskDefinition.stages.length + 1;
+    this.taskDefinition.stages.push({
+      id: newLength,
+      taskDefinitionId: this.taskDefinition.id,
+      title: `Stage ${newLength}`,
+      order: newLength - 1,
+      criteria: 'task',
+      cases: ['satisfactory'],
+    });
+    this.table.renderRows();
+  }
+
+  public removeStage(stage: Stage) {
+    this.taskDefinition.stages = this.taskDefinition.stages.filter(
+      (aStage) => aStage.id != stage.id,
+    );
+  }
+}
