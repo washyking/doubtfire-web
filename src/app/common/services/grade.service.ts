8f41cd1656971963c2035a9249acdde0a257766e
Raymond Liang <raymondL@tuta.io>
2024-05-26 16:10:53 +1000

    fix: revert index changes, html changes for grade
    revert grade indexing, edit html files to work with original indexing

--- src/app/common/services/grade.service.ts
+++ src/app/common/services/grade.service.ts
@@ -5,31 +5,31 @@ import {Project} from 'src/app/api/models/project';
   providedIn: 'root',
 })
 export class GradeService {
-  allGradeValues = [0, 1, 2, 3, 4];
-  gradeValues = [1, 2, 3, 4];
+  allGradeValues = [-1, 0, 1, 2, 3];
+  gradeValues = [0, 1, 2, 3];
 
   grades = {
-    0: 'Fail',
-    1: 'Pass',
-    2: 'Credit',
-    3: 'Distinction',
-    4: 'High Distinction',
+    '-1': 'Fail',
+    0: 'Pass',
+    1: 'Credit',
+    2: 'Distinction',
+    3: 'High Distinction',
   };
 
   gradeViewData = [
-    {value: 0, viewValue: 'Fail'},
-    {value: 1, viewValue: 'Pass'},
-    {value: 2, viewValue: 'Credit'},
-    {value: 3, viewValue: 'Distinction'},
-    {value: 4, viewValue: 'High Distinction'},
+    {value: -1, viewValue: 'Fail'},
+    {value: 0, viewValue: 'Pass'},
+    {value: 1, viewValue: 'Credit'},
+    {value: 2, viewValue: 'Distinction'},
+    {value: 3, viewValue: 'High Distinction'},
   ];
 
   public gradeNumbers = {
-    F: 0,
-    P: 1,
-    C: 2,
-    D: 3,
-    HD: 4,
+    F: -1,
+    P: 0,
+    C: 1,
+    D: 2,
+    HD: 3,
   };
 
   public gradeAcronyms = {
@@ -38,28 +38,28 @@ export class GradeService {
     Credit: 'C',
     Distinction: 'D',
     'High Distinction': 'HD',
-    0: 'F',
-    1: 'P',
-    2: 'C',
-    3: 'D',
-    4: 'HD',
+    '-1': 'F',
+    0: 'P',
+    1: 'C',
+    2: 'D',
+    3: 'HD',
   };
 
   public gradeColors = {
     // Fail
-    0: '#808080',
+    '-1': '#808080',
     F: '#808080',
     // Pass
-    1: '#FF0000',
+    0: '#FF0000',
     P: '#FF0000',
     // Credit
-    2: '#FF8000',
+    1: '#FF8000',
     C: '#FF8000',
     // Distinction
-    3: '#0080FF',
+    2: '#0080FF',
     D: '#0080FF',
     // High Distinction
-    4: '#80FF00',
+    3: '#80FF00',
     HD: '#80FF00',
   };
 
