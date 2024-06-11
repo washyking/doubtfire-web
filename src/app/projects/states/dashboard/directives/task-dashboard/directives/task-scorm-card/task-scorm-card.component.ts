703563c86253c60ad30d451ee2d8e0fa7ebbfabb
satikaj <117552851+satikaj@users.noreply.github.com>
2024-06-11 22:20:39 +1000

    feat: disable attempt button if passed and add button to review latest attempt in card

--- src/app/projects/states/dashboard/directives/task-dashboard/directives/task-scorm-card/task-scorm-card.component.ts
+++ src/app/projects/states/dashboard/directives/task-dashboard/directives/task-scorm-card/task-scorm-card.component.ts
@@ -1,5 +1,11 @@
-import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
-import {Task, User, UserService} from 'src/app/api/models/doubtfire-model';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {
+  Task,
+  TestAttempt,
+  TestAttemptService,
+  User,
+  UserService,
+} from 'src/app/api/models/doubtfire-model';
 import {ScormExtensionModalService} from 'src/app/common/modals/scorm-extension-modal/scorm-extension-modal.service';
 
 @Component({
@@ -7,23 +13,37 @@ import {ScormExtensionModalService} from 'src/app/common/modals/scorm-extension-
   templateUrl: './task-scorm-card.component.html',
   styleUrls: ['./task-scorm-card.component.scss'],
 })
-export class TaskScormCardComponent implements OnChanges {
+export class TaskScormCardComponent implements OnInit, OnChanges {
   @Input() task: Task;
   attemptsLeft: number;
+  latestCompletedAttempt: TestAttempt;
   user: User;
 
   constructor(
     private extensions: ScormExtensionModalService,
+    private testAttemptService: TestAttemptService,
     private userService: UserService,
   ) {
-    this.user = userService.currentUser;
+    this.user = this.userService.currentUser;
+  }
+
+  ngOnInit() {
+    this.refreshAttemptData();
   }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes.task && changes.task.currentValue) {
-      this.attemptsLeft = undefined;
-      this.getAttemptsLeft();
+      this.refreshAttemptData();
     }
+  }
+
+  refreshAttemptData(): void {
+    this.attemptsLeft = undefined;
+    this.getAttemptsLeft();
+    this.latestCompletedAttempt = undefined;
+    this.testAttemptService.getLatestCompletedAttempt(this.task).subscribe((attempt) => {
+      this.latestCompletedAttempt = attempt;
+    });
   }
 
   getAttemptsLeft(): void {
@@ -31,14 +51,29 @@ export class TaskScormCardComponent implements OnChanges {
       this.task.fetchTestAttempts().subscribe((attempts) => {
         let count = attempts.length;
         if (count > 0 && attempts[0].terminated === false) count--;
-        this.attemptsLeft = this.task.definition.scormAttemptLimit + this.task.scormExtensions - count;
+        this.attemptsLeft =
+          this.task.definition.scormAttemptLimit + this.task.scormExtensions - count;
       });
     }
+  }
+
+  checkIfPassed(): boolean {
+    if (this.latestCompletedAttempt) {
+      return this.latestCompletedAttempt.successStatus;
+    }
+    return false;
   }
 
   launchScormPlayer(): void {
     window.open(
       `#/projects/${this.task.project.id}/task_def_id/${this.task.taskDefId}/scorm-player/normal`,
+      '_blank',
+    );
+  }
+
+  reviewLatestCompletedAttempt(): void {
+    window.open(
+      `#/task_def_id/${this.task.taskDefId}/scorm-player/review/${this.latestCompletedAttempt.id}`,
       '_blank',
     );
   }
