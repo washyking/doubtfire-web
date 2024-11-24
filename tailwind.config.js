7c15d5a750d78a14af225a9527e8a4d300912f49
washyking <maiblefive201@gmail.com>
2024-11-24 18:33:03 +1100

    feat: changes made regarding comments in PR
    Signed-off-by: washyking <maiblefive201@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
         'formatif-blue': '#3939ff',
         'formatif-blue-lighter': '#e7e7ff',
       },
+      boxShadow: {
+        'outline-blue': '0 0 0 3px rgba(0, 123, 255, 0.5)',
+      },
     },
   },
   plugins: [],
